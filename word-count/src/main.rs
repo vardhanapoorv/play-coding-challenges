8ba8c3b3c2c292356d91a9d6086a2b56de556445
Apoorv Vardhan <vardhanapoorv@gmail.com>
2024-09-20 09:33:07 +0530

    Add ability to read from stdin when no filepath given

--- word-count/src/main.rs
+++ word-count/src/main.rs
@@ -9,59 +9,59 @@ use std::path::PathBuf;
 struct Args {
     /// File path
     #[arg(short = 'c')]
-    count_bytes_path: Option<PathBuf>,
+    count_bytes: bool,
 
     #[arg(short = 'l')]
-    count_lines_path: Option<PathBuf>,
+    count_lines: bool,
 
     #[arg(short = 'w')]
-    count_words_path: Option<PathBuf>,
+    count_words: bool,
 
     #[arg(short = 'm')]
-    count_characters_path: Option<PathBuf>,
+    count_characters: bool,
 
-    count_all_path: Option<PathBuf>,
+    file_path: Option<PathBuf>,
 }
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let file_path = args
-        .count_bytes_path
-        .as_ref()
-        .or_else(|| args.count_lines_path.as_ref())
-        .or_else(|| args.count_words_path.as_ref())
-        .or_else(|| args.count_characters_path.as_ref())
-        .or_else(|| args.count_all_path.as_ref())
-        .expect("No file path provided");
+    let input: Box<dyn Read> = if let Some(file_path) = args.file_path.as_ref() {
+        Box::new(File::open(file_path)?)
+    } else {
+        println!("No file path provided, reading from stdin");
+        Box::new(io::stdin())
+    };
 
-    let file = File::open(&file_path)?;
-    let mut buffered = BufReader::new(file);
+    let mut buffered = BufReader::new(input);
     let mut buffer: Vec<u8> = Vec::new();
     buffered.read_to_end(&mut buffer)?;
 
-    let content = String::from_utf8_lossy(&buffer);
+    let count_all = !(args.count_bytes || args.count_words || args.count_lines);
 
-    if args.count_bytes_path.is_some() || args.count_all_path.is_some() {
+    if args.count_bytes || count_all {
         let byte_count = buffer.len();
         print!("{}  ", byte_count);
     }
 
-    if args.count_characters_path.is_some() {
+    let content = String::from_utf8_lossy(&buffer);
+
+    if args.count_characters {
         let char_count = content.chars().count();
         print!("{}  ", char_count);
     }
 
-    if args.count_words_path.is_some() || args.count_all_path.is_some() {
+    if args.count_words || count_all {
         let word_count = content.split_whitespace().count();
         print!("{}  ", word_count);
     }
 
-    if args.count_lines_path.is_some() || args.count_all_path.is_some() {
+    if args.count_lines || count_all {
         let line_count = content.lines().count();
         print!("{}  ", line_count);
     }
-    print!("{}\n", file_path.display());
+
+    println!();
 
     Ok(())
 }
