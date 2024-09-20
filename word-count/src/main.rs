084b5c2b942b6b958eb5095e9e3754bee7cf66ae
Apoorv Vardhan <vardhanapoorv@gmail.com>
2024-09-20 09:00:11 +0530

    Cleanup code

--- word-count/src/main.rs
+++ word-count/src/main.rs
@@ -26,44 +26,38 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let file_path = if let Some(ref filePath) = args.count_bytes_path {
-        filePath
-    } else if let Some(ref filePath) = args.count_lines_path {
-        filePath
-    } else if let Some(ref filePath) = args.count_words_path {
-        filePath
-    } else if let Some(ref filePath) = args.count_characters_path {
-        filePath
-    } else if let Some(ref filePath) = args.count_all_path {
-        filePath
-    } else {
-        panic!("No file path provided");
-    };
+    let file_path = args
+        .count_bytes_path
+        .as_ref()
+        .or_else(|| args.count_lines_path.as_ref())
+        .or_else(|| args.count_words_path.as_ref())
+        .or_else(|| args.count_characters_path.as_ref())
+        .or_else(|| args.count_all_path.as_ref())
+        .expect("No file path provided");
 
     let file = File::open(&file_path)?;
     let mut buffered = BufReader::new(file);
     let mut buffer: Vec<u8> = Vec::new();
     buffered.read_to_end(&mut buffer)?;
+
+    let content = String::from_utf8_lossy(&buffer);
+
     if args.count_bytes_path.is_some() || args.count_all_path.is_some() {
         let byte_count = buffer.len();
         print!("{}  ", byte_count);
     }
 
     if args.count_characters_path.is_some() {
-        // let char_count = buffer.iter().filter(|&&b| b.is_ascii()).count();
-        let content = String::from_utf8_lossy(&buffer);
         let char_count = content.chars().count();
         print!("{}  ", char_count);
     }
 
     if args.count_words_path.is_some() || args.count_all_path.is_some() {
-        let content = String::from_utf8_lossy(&buffer);
         let word_count = content.split_whitespace().count();
         print!("{}  ", word_count);
     }
 
     if args.count_lines_path.is_some() || args.count_all_path.is_some() {
-        let content = String::from_utf8_lossy(&buffer);
         let line_count = content.lines().count();
         print!("{}  ", line_count);
     }
