69de594c7c5419a01f28df3aa470737bb4a3adf4
Apoorv Vardhan <vardhanapoorv@gmail.com>
2024-09-26 20:57:03 +0530

    Json parser - bit of cleanup & step 3 done

--- json-parser/src/main.rs
+++ json-parser/src/main.rs
@@ -8,53 +8,85 @@ struct Args {
     object: String,
 }
 
+struct SpecialChars;
+
+impl SpecialChars {
+    pub const OPEN_BRACE: char = '{';
+    pub const CLOSE_BRACE: char = '}';
+    pub const COMMA: char = ',';
+    pub const COLON: char = ':';
+    pub const QUOTE: char = '"';
+}
+
 fn main() {
     let args = Args::parse();
     let mut stack = Vec::<char>::new();
     let mut is_valid_json = false;
     let mut key_str = String::new();
-    // let mut map = std::collections::HashMap::new();
     let mut keys = Vec::<String>::new();
+    let mut is_non_quoted_value = false;
 
     for val in args.object.chars() {
-        //println!("val: {}", val);
-        if val == '}' && stack.last().unwrap() == &'{' {
+        if val == SpecialChars::CLOSE_BRACE && *stack.last().unwrap() == SpecialChars::OPEN_BRACE {
             if key_str.len() > 0 {
                 keys.push(key_str);
-                key_str = String::new();
             }
             is_valid_json = true;
             break;
         } else if val == ' ' || val == '\n' || val == '\t' {
             continue;
-        } else if val == '"' && *stack.last().unwrap() == '"' {
+        } else if val == SpecialChars::QUOTE && *stack.last().unwrap() == SpecialChars::QUOTE {
             stack.pop();
-            println!("stack {:?}", stack);
             continue;
-        } else if val == '"' {
-            if *stack.last().unwrap() == ':' || *stack.last().unwrap() == ',' {
+        } else if val == SpecialChars::QUOTE {
+            if *stack.last().unwrap() == SpecialChars::COLON
+                || *stack.last().unwrap() == SpecialChars::COMMA
+            {
                 stack.pop();
             }
             stack.push(val);
             continue;
-        } else if !stack.is_empty() && *stack.last().unwrap() == '"' {
+        } else if !stack.is_empty() && *stack.last().unwrap() == SpecialChars::COLON {
+            println!("stack {:?}", stack);
+            stack.pop();
+            key_str += val.to_string().as_str();
+            is_non_quoted_value = true;
+            continue;
+        } else if !stack.is_empty() && *stack.last().unwrap() == SpecialChars::QUOTE {
             key_str += val.to_string().as_str();
             println!("key_str {:?}", key_str);
             continue;
-        } else if val == ':' {
-            println!("key_str {:?}", key_str);
+        } else if val == SpecialChars::COLON {
             if key_str.len() > 0 {
                 keys.push(key_str);
                 key_str = String::new();
             }
             stack.push(val);
             continue;
-        } else if val == ',' {
+        } else if val == SpecialChars::COMMA {
             if key_str.len() > 0 {
-                keys.push(key_str);
-                key_str = String::new();
+                if is_non_quoted_value {
+                    if key_str.parse::<i32>().is_ok()
+                        || key_str == "true"
+                        || key_str == "false"
+                        || key_str == "null"
+                    {
+                        is_non_quoted_value = false;
+                        keys.push(key_str);
+                        key_str = String::new();
+                    } else {
+                        is_valid_json = false;
+                        break;
+                    }
+                } else {
+                    keys.push(key_str);
+                    key_str = String::new();
+                }
             }
             stack.push(val);
+            continue;
+        } else if is_non_quoted_value {
+            key_str += val.to_string().as_str();
             continue;
         } else if val.is_alphanumeric() {
             is_valid_json = false;
@@ -63,12 +95,6 @@ fn main() {
         stack.push(val);
     }
 
-    if keys.len() > 0 {
-        // map.insert(keys[0].clone(), keys[1].clone());
-    }
-    // map.insert(keys[0].clone(), keys[1].clone());
-    // map.insert("d", "d");
-    // println!("map: {:?}", map);
     println!("stack: {:?}", stack);
     println!("keys: {:?}", keys);
 
